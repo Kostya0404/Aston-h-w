aae96b05d50a535168362b8097646c227e13b58e
Kostya0404 <kostyan66.sys@gmail.com>
2025-01-25 20:41:10 +0300

    Homework

---
+++ src/main/java/Lesson_8/WordsCounter.java
@@ -0,0 +1,20 @@
+package Lesson_8;
+
+import java.util.*;
+
+public class WordsCounter {
+    public static void main(String[] args) {
+        String[] arr = {"Автомобиль", "Насос", "Лестница", "Кочерга", "Кровать", "Кочерга", "Окно", "Автомобиль", "Кровать", "Культиватор", "Кровать", "Кочерга", "Окно", "Жалюзи", "Насос", "Компьютер", "Жалюзи", "Расческа", "Картина", "Картина", "Картина", "Картина", "Картина"};
+
+        Map<String, Integer> wordCount = new HashMap<>();
+
+        for (String word : arr) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+
+        System.out.println("Cлова и их количество");
+        for (Map.Entry<String, Integer> e : wordCount.entrySet()) {
+            System.out.println(e.getKey() + " " + e.getValue());
+        }
+    }
+}
