d9a5747b8e94c84a32a8a4c58a8f78069a79e05f
Kostya0404 <kostyan66.sys@gmail.com>
2025-01-29 22:26:54 +0300

    homework3

--- src/main/java/Lesson_8/WordsCounter.java
+++ src/main/java/Lesson_8/WordsCounter.java
@@ -7,22 +7,19 @@ public class WordsCounter {
         String[] arr = {"Автомобиль", "Насос", "Лестница", "Кочерга", "Кровать", "Кочерга", "Окно", "Автомобиль", "Кровать", "Культиватор", "Кровать", "Кочерга", "Окно", "Жалюзи", "Насос", "Компьютер", "Жалюзи", "Расческа", "Картина", "Картина", "Картина", "Картина", "Картина"};
 
         Map<String, Integer> wordCount = new HashMap<>();
-
         for (String word : arr) {
             wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
         }
-
         System.out.println("Cлова и их количество");
         for (Map.Entry<String, Integer> e : wordCount.entrySet()) {
             System.out.println(e.getKey() + " " + e.getValue());
         }
-        System.out.println("\nСписок уникальных слов:");
-        printUniqueWords(arr);
-    }
-    public static void printUniqueWords(String[] arr) {
-        Set<String> uniqueWords = new HashSet<>(Arrays.asList(arr));
-        for (String word : uniqueWords) {
-            System.out.println(word);
+
+        System.out.println("\nУникальные слова:");
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            if (entry.getValue() == 1) {
+                System.out.println(entry.getKey());
+            }
         }
     }
 }
